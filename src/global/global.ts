dce0c8ca92f099da9be79d222d95cdde9c4b39cd
姚成成 <378186268@qq.com>
2022-01-09 20:30:45 +0800

    处理nginx反向代理

--- src/global/global.ts
+++ src/global/global.ts
@@ -1,3 +1,4 @@
 export default {
-  defaultPath: process.env.NODE_ENV === 'production' ? '' : 'lovediary'
+  // defaultPath: process.env.NODE_ENV === 'production' ? '' : 'lovediary'
+  defaultPath: 'lovediary' // nginx代理地址
 };
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -4,6 +4,16 @@ const service = axios.create({
   timeout: 10000
 });
 
+// 请求拦截
+service.interceptors.request.use(config => {
+  console.log(config);
+  // if (process.env.NODE_ENV === 'production') {
+  //   const url = window.location.origin + ':9999';
+  //   config.url = url + config.url;
+  // }
+  return config;
+});
+
 const http = {
   post: (path: string, params: { username: string; password: string }) => {
     return service.post(path, params);
